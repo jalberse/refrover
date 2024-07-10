a4c0a5212b7fb117656fc239c88be15db220c1f0
John Alberse <alberse.john@gmail.com>
2024-07-10 17:21:29 -0500

    Enable Tauri sql plugin
    We will be using SQLite, which is the best fit for the local
    application IMO. At the least, I am most familiar with
    it and know it will be sufficient.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,6 +17,7 @@ fn on_button_clicked() -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .invoke_handler(tauri::generate_handler![on_button_clicked])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
