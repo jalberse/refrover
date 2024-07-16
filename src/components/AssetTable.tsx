5608d46e87c8d3fdef7f9f42c04653ec0f4accd7
John Alberse <alberse.john@gmail.com>
2024-07-16 16:34:02 -0500

    Some comments

--- src/components/AssetTable.tsx
+++ src/components/AssetTable.tsx
@@ -7,6 +7,11 @@ export const AssetTable: React.FC<AssetTableProps> = ({
   search_text,
   current_page,
 }: AssetTableProps) => {
+  // TODO Fetch data from our Rust backend and display based on the query.
+
+  // TODO Remove the current_page stuff, I won't bother with pagination
+  //    since I'll try to jump straight to infinite scroll. Until then, smaller data sets.
+
   return (
     <div className="flex flex-1 flex-col items-center justify-center py-8">
       <h1 className="m-0 text-center text-6xl">{search_text}</h1>
