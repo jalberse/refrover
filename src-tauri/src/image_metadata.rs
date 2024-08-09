f1256d8cca79f1d97f906b10727c7abeed762f53
John Alberse <alberse.john@gmail.com>
2024-08-09 15:05:38 -0500

    Remove comment

--- src-tauri/src/image_metadata.rs
+++ src-tauri/src/image_metadata.rs
@@ -8,8 +8,6 @@ pub struct ImageSize
     pub height: u32,
 }
 
-// TODO I'm just going to fork imghdr, add a serde feature, and use that instead of this. I'll also submit a pull request.
-
 /// The metadata for a file; currently, image files.
 /// This may include e.g. EXIF metadata, but also metadata from RefRover such as the file ID.
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
