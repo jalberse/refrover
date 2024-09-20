d87a643d75918f7b8e72dabdbb3fa65c0006be8e
John Alberse <alberse.john@gmail.com>
2024-09-20 12:51:18 -0500

    WIP Display trees for directories
    fs and path are failing to import, however. I believe because
    of nextjs.
    
    We should likely just move to the tauri fs api.

--- src/components/WatchedDirectories.tsx
+++ src/components/WatchedDirectories.tsx
@@ -1,109 +1,153 @@
+'use client'
+
 import { open } from "@tauri-apps/api/dialog"
 import type React from "react"
 import { useEffect, useRef, useState } from "react"
 import { addWatchedDirectory, deleteWatchedDirectory } from "../api"
 import WatchedDirectoriesList from "./WatchedDirectoriesList"
 
+import * as fs from 'fs';
+import * as path from 'path';
+import { RichTreeView } from "@mui/x-tree-view"
+
+
 export type Directory = {
   id: number
   path: string
 }
 
-const WatchedDirectories: React.FC = () => {
-  // TODO We need to fetch the initial set of watched directories from the backend
-  // TODO Note we may add directories if we e.g. drag files from file explorer in (we'd add its parent)
-  const [directories, setDirectories] = useState<Directory[]>([])
-  const prevDirectoriesRef = useRef<Directory[]>([])
 
-  // TODO I think we want to display the tree for each watched directory instead. Users should be able to click through the trees of each watched directory to see the files within.
-  //      See comment at https://vizlib-io.atlassian.net/browse/ROVER-84
-  //      I don't think we need to like, include those subdirs in our database or anything.
-  //      We should be able to display the tree here (listening for new subdirs though?)
-  //      and then for search, we can search by *prefix* of the dirs most likely?
-  // https://stackoverflow.com/questions/15020690/sqlilte-searching-with-prefix-string-by-using-match
-  //      Yeah, that basically seems possible/fine.
-  // Also, reference the Kindle search (and probably some other search aparatuses) for how we'd want to convey this.
-  //      I'm thinking something like a "search" icon next to each dir, and then when you click it,
-  //      a little bubble under our natural language search bar appears with "Starting With: [dir path]"
-  //      Those are all treated as ORs I suppose.
-  //      So you click in this hierarchy to add/remove dirs to search, and below the search bar we show
-  //      which are active. Empty search + no dirs, empty gallery. Empty search + dir, images in that dir.
-  //      Search + active dir, we only search in those active dirs.
-  //      The *ordering* will come from the natural language search (if applicable).
-  //      Tags, hen we add them after the MVP, will function similarly.
-  // TODO But consider if a new dir is added to the watched directory.
-  //      In our notify handler, we should probably just send an event any time a directory changes
-  //      (rename, move, add, delete, etc.) and then on that event, we re-build this tree?
-  //      We could track the subdirs in the database and then construct the tree from that, too...
-  //      Not sure.
-  //      Also - if a directory is removed, does it still fire all the file removal events?
-  //       (or if it's added, does it fire all the file add events?)
-  //       If the file events still trigger that's ideal, since we can just work with their absolute paths
-  //       and say "it's in this watched directory" and not worry about subdir structure on the backend.
-  //       We'd only need to worry about subdir structure for prefix searching, which is a simpler problem.
+type DirectoryTreeItem = {
+  // TODO Perhaps generate IDs instead; we do string comparisons, but I'm not about to optimize for that yet.
+  // The full path of the directory. Can be used as a unique ID.
+  path: string;
+  // TODO - potentially delete, and just call path.basename(path) where necessary?
+  // The label to display in the hierarchy
+  label: string;
+  children?: DirectoryTreeItem[];
+};
 
-  // TODO hmm. gross.
-  const generateUniqueId = () => {
-    return Math.floor(Math.random() * 1000000)
+function getDirectoryTrees(directories: string[]): DirectoryTreeItem[] {
+  const tree: DirectoryTreeItem[] = [];
+
+  directories.forEach((dir) => {
+    const dirTree = buildTree(dir);
+    if (dirTree) {
+      tree.push(dirTree);
+    }
+  });
+
+  return tree;
+}
+
+function buildTree(dirPath: string): DirectoryTreeItem | null {
+  const stats = fs.statSync(dirPath);
+  if (!stats.isDirectory()) {
+    return null;
   }
 
+  const dirName = path.basename(dirPath);
+  const treeItem: DirectoryTreeItem = {
+    path: dirPath,
+    label: dirName,
+    children: [],
+  };
+
+  const items = fs.readdirSync(dirPath);
+  items.forEach((item) => {
+    const itemPath = path.join(dirPath, item);
+    const itemStats = fs.statSync(itemPath);
+
+    if (itemStats.isDirectory()) {
+      const childTree = buildTree(itemPath);
+      if (childTree) {
+        treeItem.children?.push(childTree);
+      }
+    }
+  });
+
+  return treeItem;
+}
+
+const WatchedDirectories: React.FC = () => {
+  // TODO We need to fetch the initial set of watched directories from the backend
+  //      That will use getDirectoryTrees(), and should be in a useEffect() on mount (ie with empty dependency list)
+  // TODO Note we may add directories if we e.g. drag files from file explorer in (we'd add its parent)
+  //      (actually don't do that, but I think we *do* want a default directory. I think we'd handle that in the backend, though
+  //       and we'd get that on mount from the DB from the above TODO)
+
+  const [directoryTrees, setDirectoryTrees] = useState<DirectoryTreeItem[]>([]);
+  const prevDirectoryTreesRef = useRef<DirectoryTreeItem[]>([]);
+
+  // TODO We'll have DirectoryTreeItem[] in the state.
+  //      We'll then use that to render the trees in the WatchedDirectoriesList component, modifying it to be like TagHierarchy to display those.
+  //      On mount, we should fetch the directories from the backend and then build the tree from that.
+  //      We can also update via the fs dialog to add a new watched directory.
+  //      Finally, we'll also send an event from the fs watcher on the backend so that if a directory is added or removed within a watched directory,
+  //          we can update the tree accordingly. We can probably just rebuild the whole thing, at least at first (we can surely send hints in the event though,
+  //          just the path is good since we can jump to that location and insert it, I guess)
+  // Once we have that, we can add directories to search by constructing the prefix search query from the tree and filtering the search results accordingly (URL query params).
+  //      Buttons to add/remove dirs from search etc.
+  //      SQL LIKE queries for prefix search make this simple.
+  //      This is all far easier than explicitly tracking subdirs in the database, I think.
+  // https://vizlib-io.atlassian.net/browse/ROVER-84
+
   const addDirectory = async () => {
-    const selectedPath = await open({
+    let selectedPath = await open({
       directory: true,
       multiple: true,
       title: "Select a directory",
     })
 
     if (selectedPath && typeof selectedPath === "string") {
-      const newDirectory: Directory = {
-        id: generateUniqueId(),
-        path: selectedPath,
-      }
-      setDirectories([...directories, newDirectory])
+      // Only one selected path; make it an array and fall through
+      selectedPath = [selectedPath]
     }
     if (selectedPath && Array.isArray(selectedPath)) {
-      const newDirectories = selectedPath.map((path) => ({
-        id: generateUniqueId(),
-        path,
-      }))
-      setDirectories([...directories, ...newDirectories])
+      const newDirectories = selectedPath.map((path) => {
+        // Call buildTree() to get the DirectoryTreeItem
+        const tree = buildTree(path);
+        return tree;
+      })
+      .filter((dir) => dir !== null)
+      setDirectoryTrees([...directoryTrees, ...newDirectories])
     }
   }
 
-  const removeDirectory = (id: number) => {
-    const newDirectories = directories.filter(
-      (directory) => directory.id !== id,
+  const removeDirectory = (path: string) => {
+    const newDirectories = directoryTrees.filter(
+      (directory) => directory.path !== path,
     )
-    setDirectories(newDirectories)
+    setDirectoryTrees(newDirectories)
   }
 
   useEffect(() => {
-    const prevDirectories = prevDirectoriesRef.current
+    const prevDirectories = prevDirectoryTreesRef.current
 
-    const addedDirectories = directories.filter(
-      (dir) => !prevDirectories.some((prevDir) => prevDir.id === dir.id),
+    const addedDirectories = directoryTrees.filter(
+      (dir) => !prevDirectories.some((prevDir) => prevDir.path === dir.path),
     )
 
     const removedDirectories = prevDirectories.filter(
-      (prevDir) => !directories.some((dir) => dir.id === prevDir.id),
+      (prevDir) => !directoryTrees.some((dir) => dir.path === prevDir.path),
     )
 
-    // TODO Do we need to format the paths at all?
     const addPromises = addedDirectories.map((dir) =>
       addWatchedDirectory(dir.path),
     )
+
     const removePromises = removedDirectories.map((dir) =>
       deleteWatchedDirectory(dir.path),
     )
 
     Promise.all([...addPromises, ...removePromises])
-      .then(() => {
-        prevDirectoriesRef.current = directories
-      })
-      .catch((error: unknown) => {
-        console.error("Error updating watched directories:", error)
-      })
-  }, [directories])
+       .then(() => {
+        prevDirectoryTreesRef.current = directoryTrees
+       })
+       .catch((error: unknown) => {
+         console.error("Error updating watched directories:", error)
+       })
+  }, [directoryTrees])
 
   return (
     <div>
@@ -118,10 +162,13 @@ const WatchedDirectories: React.FC = () => {
       >
         Add Directory
       </button>
-      <WatchedDirectoriesList
-        directories={directories}
-        removeDirectory={removeDirectory}
-      />
+      <RichTreeView items={directoryTrees} />
+      {
+        // <WatchedDirectoriesList
+        // directories={directories}
+        // removeDirectory={removeDirectory}
+        // />
+      }
     </div>
   )
 }
