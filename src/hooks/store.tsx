92caa68aefc88b7ba0cb3ecee4583370fda8a6ef
John Alberse <alberse.john@gmail.com>
2024-09-25 14:26:06 -0500

    Remove fsEvent store, some comments
    The fsEvent store is replaced by the taskStatuses

--- src/hooks/store.tsx
+++ src/hooks/store.tsx
@@ -5,23 +5,8 @@ type RoverStore = {
   setDetailsViewFileUuid: (uuid: string) => void
   clearDetailsViewFileUuid: () => void
 
-  // TODO Delete the fsEventStatus stuff... Task bar will now use taskStatuses.
-  fsEventStatus: string
-  setFsEventStatus: (status: string) => void
-  clearFsEventStatus: () => void
-
   pathPrefixes: string[]
   setPathPrefixes: (prefixes: string[]) => void
-
-  // TODO I'm thinking:
-  // taskStatuses empty? "rover-analyzer (checkmark)"
-  // taskStatuses non-empty? "rover-analyzer (spinner)"
-  // Clicking the rover-analyzer will then show each of the "status" strings in a little
-  //  modal list above it. The status strings from the backend can include the
-  //  description and the numebr of images processed out of total.
-  //  For now, that can just be in the given status string - the backend can format.
-  //  In the future we might want to have a more structured status object
-  //  so we can display things differently, but don't bother for now.
 
   // Create a map from UUIDs to statuses.
   taskStatuses: Map<string, string>
@@ -38,14 +23,6 @@ const useRoverStore = create<RoverStore>((set) => ({
     set(() => ({ detailsViewFileUuid: "" }))
   },
 
-  fsEventStatus: "rover-analyzer",
-  setFsEventStatus: (status) => {
-    set(() => ({ fsEventStatus: status }))
-  },
-  clearFsEventStatus: () => {
-    set(() => ({ fsEventStatus: "" }))
-  },
-
   pathPrefixes: [],
   setPathPrefixes: (prefixes) => {
     set(() => ({ pathPrefixes: prefixes }))
@@ -54,6 +31,10 @@ const useRoverStore = create<RoverStore>((set) => ({
   taskStatuses: new Map(),
   setTaskStatus: (uuid, status) => {
     set((state) => {
+      // Note that we make a copy of the map here.
+      // This is because updates to Zustand states must be
+      // *immutable*. If we were to set() the original map,
+      // we would not trigger a re-render.
       const newTaskStatuses = new Map(state.taskStatuses)
       newTaskStatuses.set(uuid, status)
       return { taskStatuses: newTaskStatuses }
